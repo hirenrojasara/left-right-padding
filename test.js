92c940638b84588221d365e07026b7ab136373ae
Hiren <hiren.soni@rapidops.com>
2016-04-21 13:18:31 +0530

    Second Test Case

--- test.js
+++ test.js
@@ -2,17 +2,19 @@ var padding = require("./index");
 var assert = require('assert');
 
 describe('Left Padding', function () {
-    it('shoud run *test', function() {
+    it('shoud be *test', function() {
         var val = padding.lpad("test", "*");
         assert.equal(val, "*test");
     });
 
+    it('shoud be **********test', function() {
+        var val = padding.lpad("test", "*", 10)
+        assert.equal(val, "**********test");
+    });
+
 });
 
-console.log ( padding.lpad("test", "*"));
-//Output:	*test
-
-console.log ( padding.lpad("test", "*", 10));
+console.log ( );
 //Output:	**********test
 
 console.log ( padding.lpad("test", "*", 3, 15));
